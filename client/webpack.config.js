2a0370b14794f3b0445866ad70d9ef96e504fbea
KGBerry542 <74325710+KGBerry542@users.noreply.github.com>
2023-07-19 21:07:36 -0400

    Configure webpack for PWA, manifest, CSS, and Babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,12 +16,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './client/index.html',
+        filename: 'index.html',
+        chunks: ['main'],
+      }),
+
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'A simple text editor Progressive Web App.',
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('client/assets/icons/icon_96x96.png'),
+            sizes: [72, 96, 128, 144, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),  
+
+      new InjectManifest({
+        swSrc: './client/src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },  
       ],
     },
   };
